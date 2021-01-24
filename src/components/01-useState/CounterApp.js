5f8778f68f9a576645a9e8175a52b05fafa0b867
Osama Moussati Lamhamdi <66735418+blueC4P@users.noreply.github.com>
2021-01-24 00:07:01 +0000

    Contador basico con useState

---
+++ src/components/01-useState/CounterApp.js
@@ -0,0 +1,26 @@
+import React, {useState} from 'react'
+import "./Counter.css"
+
+export const CounterApp = () => {
+
+
+    const [counter, setCounter] = useState(10)
+
+    return (
+        <>
+            <h1>Counter {counter}</h1>
+            <hr/>
+
+            <button 
+            className="btn btn-primary"
+            onClick={
+                () => {
+                    setCounter(counter + 1)
+                }
+            }
+            >
+             +1
+            </button>   
+        </>
+    )
+}
