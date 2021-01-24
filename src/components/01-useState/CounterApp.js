b0fed0d127704d864de86e964fcd433e811323b4
Osama Moussati Lamhamdi <66735418+blueC4P@users.noreply.github.com>
2021-01-24 00:24:38 +0000

    Trabajar con varios estados y editarlos con el manejo de los datos

--- src/components/01-useState/CounterApp.js
+++ src/components/01-useState/CounterApp.js
@@ -4,23 +4,51 @@ import "./Counter.css"
 export const CounterApp = () => {
 
 
-    const [counter, setCounter] = useState(10)
+    const [state, setState] = useState({
+        counter1: 10,
+        counter2: 20,
+        counter3: 30,
+        counter4: 40
+    });
+
+    const {counter1, counter2} = state;
 
     return (
         <>
-            <h1>Counter {counter}</h1>
+            <h1>Counter 1  {counter1}</h1>
+            <h1>Counter 2  {counter2}</h1>
             <hr/>
 
             <button 
             className="btn btn-primary"
             onClick={
                 () => {
-                    setCounter(counter + 1)
+                    setState(
+                        {
+                            ...state,
+                            counter1: counter1 + 1
+                        }
+                    )
                 }
             }
             >
-             +1
-            </button>   
+             +1 counter1
+            </button>
+            <button 
+            className="btn btn-primary"
+            onClick={
+                () => {
+                    setState(
+                        {
+                            ...state,
+                            counter2: counter2 + 1
+                        }
+                    )
+                }
+            }
+            >
+             +1 counter2
+            </button>
         </>
     )
 }
