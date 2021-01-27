147ba1e2f8d48b846fdce7b131f064a14fab645d
blueC4P <osamag4g@gmail.com>
2021-01-27 08:06:55 +0000

    Manejo formulario con custom hook

---
+++ src/hooks/useForm.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react'
+
+export const useForm = (initialState = {}) => {
+    const [values, setValues] = useState(initialState)
+
+    const handleInputChange = ({ target }) => {
+        setValues({
+            ...values,
+            [target.name]: target.value //target.name es un identificador de input por texto
+        })
+    }
+
+    return [values, handleInputChange]
+
+}
