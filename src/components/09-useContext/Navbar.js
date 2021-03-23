e3a4332f2c0137501211910c466f636f6b937c9f
blueC4P <osamag4g@gmail.com>
2021-03-23 21:54:55 +0000

    Navlink with activeClassName implemented

--- src/components/09-useContext/Navbar.js
+++ src/components/09-useContext/Navbar.js
@@ -1,20 +1,27 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
 export const Navbar = () => {
-    return (
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/about">About</Link>
-                </li>
-                <li>
-                    <Link to="/login">Login</Link>
-                </li>
-            </ul>
-        </nav>
-    )
-}
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <Link to="/" className="navbar-brand">
+          useContext
+        </Link>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <NavLink exact activeClassName="active" to="/" className="nav-link">
+              Home
+            </NavLink>
+            <NavLink exact activeClassName="active" to="/about" className="nav-link">
+              About
+            </NavLink>
+            <NavLink exact activeClassName="active" to="/login" className="nav-link">
+              Login
+            </NavLink>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
