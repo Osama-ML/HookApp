933a9dfc61cc4c37ab2f15e29439651302143a30
blueC4P <osamag4g@gmail.com>
2021-03-23 21:17:46 +0000

    Add routed

--- src/components/09-useContext/AppRouter.js
+++ src/components/09-useContext/AppRouter.js
@@ -1,24 +1,20 @@
-import React from 'react'
-import { AboutScreen } from './AboutScreen';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route
-  } from "react-router-dom";
-
+import React from "react";
+import { AboutScreen } from "./AboutScreen";
+import { LoginScreen } from "./LoginScreen";
+import { HomeScreen } from "./HomeScreen";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 export const AppRouter = () => {
-    return (
-        <Router>
-            <div>
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" component={HomeScreen} />
+          <Route exact path="/about" component={AboutScreen} />
+          <Route exact path="/login" component={LoginScreen} />
 
-
-                <Switch>
-                    <Route path="/about" component={AboutScreen}/>
-                </Switch>
-
-
-            </div>
-        </Router>
-    )
-}
+        </Switch>
+      </div>
+    </Router>
+  );
+};
